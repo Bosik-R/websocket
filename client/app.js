66fbf5bc0b5552eaf8b93a1717e8fdfa57867539
Tomasz Rosik <Bosik27@gmail.com>
2020-11-03 21:37:41 +0100

    Add scripts on handle login, message sending

---
+++ client/app.js
@@ -0,0 +1,62 @@
+const loginForm = document.getElementById('welcome-form');
+const messegesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+messageContentInput.autocomplete = 'off';
+
+let userName;
+
+const login = (e) => {
+  e.preventDefault();
+  if(userNameInput.value.length > 0) {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messegesSection.classList.add('show');
+  } else {
+    alert('Please enter user name')
+  }
+};
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if(messageContentInput.value.length > 0) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  } else {
+    alert('please type a message')
+  }
+};
+
+addMessage = (author, content) => {
+  const message = document.createElement('li');
+  const messageAuthor = document.createElement('H3');
+  const messageContent = document.createElement('DIV');
+
+  message.classList.add('message', 'message--received');
+  messageAuthor.classList.add('message__author');
+  messageContent.classList.add('message__content');
+  
+  if(author === userName){
+    message.classList.add('message--self')
+    messageAuthor.innerHTML = 'You';
+  } else {
+    messageAuthor.innerHTML = author;
+  }
+  
+  messageContent.innerHTML = content;
+
+  message.appendChild(messageAuthor);
+  message.appendChild(messageContent);
+
+  messagesList.appendChild(message);
+}
+
+loginForm.addEventListener('submit', function(e){
+  login(e);
+});
+
+addMessageForm.addEventListener('submit', function(e){
+  sendMessage(e);
+});
